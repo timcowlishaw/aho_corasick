fff55c86519180ff06425cda85346fee3032dcf5
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2014-10-02 15:12:52 +0100

    dont commit build artefacts

--- pkg/aho_corasick-0.0.2/spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-$: << File.join(File.dirname(__FILE__),"..")
-require 'rubygems'
-require 'bundler/setup'
-require 'lib/aho_corasick'
-require 'minitest/spec'
-require 'minitest/benchmark'
-require 'minitest/autorun'
-
